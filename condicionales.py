f9f61acd76c946d7d3be8ba4b4a24021ce7cf905
andresfvb <barandicandres@gmail.com>
2021-09-12 18:07:19 -0500

    Punto #2

--- condicionales.py
+++ condicionales.py
@@ -4,6 +4,7 @@ Created on Sun Sep 12 16:45:54 2021
 
 @author: Andres Vasquez
 """
+from random import randrange
 
 # Punto 1 --------------------------------------------------------------------
 
@@ -26,3 +27,20 @@ print(f"\nUsted compro {cant_camisas} prendas\n"
       f"Precio antes de descuento: ${precio+precio_aux}\n"
       f"Descuento del {descuento}: ${precio_aux}\n"
       f"Dando un total a pagar de: ${precio}")
+
+# Punto 2 --------------------------------------------------------------------
+
+
+precio_compra = int(input("Valor de la compra: $"))
+descuento = randrange(0, 100)
+if descuento < 74:
+    precio_aux = precio_compra*0.15
+    descuento_text = "15%"
+else:
+    precio_aux = precio_compra*0.20
+    descuento_text = "20%"
+precio_compra = precio_compra - precio_aux
+print(f"\nPrecio compra: ${precio_compra+precio_aux}"
+      f"\nBalota sacada: {descuento}"
+      f"\nDescuento ganado {descuento_text}: ${precio_aux}"
+      f"\nTotal a pagar: ${precio_compra}")
